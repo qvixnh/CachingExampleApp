20116a7585ae65ee4f13328ac8bd096046f1ba2f
qvixnh <nguyenvinh22012002@gmail.com>
2023-08-31 12:51:02 +0700

    async get employees

--- DataAccessLibrary/SampleDataAccess.cs
+++ DataAccessLibrary/SampleDataAccess.cs
@@ -20,5 +20,19 @@ namespace DataAccessLibrary
 
             return output;
         }
+        public async Task<List<EmployeeModel>> GetEmployeesAsync()
+        {
+            List<EmployeeModel> output = new();
+
+            output.Add(new() { FirstName = "Tim", LastName = "Corey" });
+            output.Add(new() { FirstName = "Sue", LastName = "Storm" });
+            output.Add(new() { FirstName = "Jane", LastName = "Jones" });
+
+            await Task.Delay(3000);//asynchronously call this awaiting
+
+            return output;
+        }
+
+
     }
 }
